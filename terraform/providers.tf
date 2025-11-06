214d2f5670630cc47537d0ae609b8b659ddf40cf
doxajay <99846671+doxajay@users.noreply.github.com>
2025-11-05 17:50:45 -0700

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,3 @@
-# =====================================================
-# Providers (AWS + Kubernetes wired to EKS)
-# =====================================================
 terraform {
   required_version = ">= 1.4.0"
 
@@ -20,7 +17,6 @@ provider "aws" {
   region = var.region
 }
 
-# Data sources to discover the EKS API details after the cluster is created
 data "aws_eks_cluster" "this" {
   name       = aws_eks_cluster.main.name
   depends_on = [aws_eks_cluster.main]
@@ -31,7 +27,6 @@ data "aws_eks_cluster_auth" "this" {
   depends_on = [aws_eks_cluster.main]
 }
 
-# IMPORTANT: this points Terraform's Kubernetes provider to your EKS API
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   token                  = data.aws_eks_cluster_auth.this.token
